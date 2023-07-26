6993771f780a71da9b8376903ede9c7cd845a62c
xavier <xavier.julia7@orange.fr>
2023-07-26 16:24:50 +0200

    added "no description" case and stock indicator

--- pieces.js
+++ pieces.js
@@ -11,9 +11,16 @@ nomElement.innerHTML = article.nom;
 const prixElement = document.createElement("p");
 prixElement.innerText = `Prix: ${article.prix}€ (${article.prix < 35 ? "€" : "€€€"})`;
 const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie;
+categorieElement.innerText = article.categorie ?? "Pas de description pour le moment.";
+const disponibility = document.createElement("p");
+if(article.disponibilite === true){
+    disponibility.innerText = "En stock"
+}else if(article.disponibilite === false){
+    disponibility.innerText = "Rupture de stock"
+};
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(disponibility)
