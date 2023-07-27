a564e207f63659aa59b670b81c5cc8692a089f33
xavier <xavier.julia7@orange.fr>
2023-07-27 10:31:59 +0200

    added filter and sort button in html file, did the js script corresponding

--- pieces.js
+++ pieces.js
@@ -29,3 +29,78 @@ for(let i = 0; i < pieces.length; i++){
     pieceElement.appendChild(descriptionElement);
     pieceElement.appendChild(disponibility);
 }
+
+//on trie les pièces par prix croissant
+const boutonTrier = document.querySelector(".btn-trier");
+boutonTrier.addEventListener("click",function(){
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function(a,b){
+        return a.prix-b.prix
+    });
+});
+
+//on trie les pièces par prix décroissant
+const boutonTrierDecroi = document.querySelector(".btn-trierDecroi");
+boutonTrierDecroi.addEventListener("click",function(){
+    const piecesOrdonneesDecroi = Array.from(pieces);
+    piecesOrdonneesDecroi.sort(function(a,b){
+        return b.prix-a.prix
+    });
+});
+
+
+//on filtre les pièces et enlèvent celles qui coûtent plus de 35€
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+boutonFiltrer.addEventListener("click", function(){
+    const piecesFiltrees = pieces.filter(function (piece){
+        return piece.prix <= 35
+    })
+    console.log(piecesFiltrees)
+});
+
+//on filtre les pièces et enlèvent celles qui n'ont pas de description
+const boutonNoDescription = document.querySelector(".btn-noDescription");
+boutonNoDescription.addEventListener("click", function(){
+    const piecesDescription = pieces.filter(function (piece){
+        return piece.description
+    })
+    console.log(piecesDescription)
+});
+
+//obtention de la liste des noms de pièces dont prix < 35€
+const noms = pieces.map(pieces => pieces.nom);
+for(let i = pieces.length -1; i >= 0; i--){
+    if(pieces[i].prix > 35){
+        noms.splice(i,1)
+    }
+};
+
+//crétion liste des pièces dont prix<35€
+const abordablesElements = document.createElement('ul');
+for(let i=0; i<noms.length; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement)
+};
+document.querySelector('.abordables').appendChild(abordablesElements)
+
+
+//obtention de la liste des pièces disponible et leur prix
+const nomsDisponibles = pieces.map(pieces => pieces.nom);
+const prixDisponibles = pieces.map(pieces => pieces.prix);
+for(let i=pieces.length -1; i>= 0; i--){
+    if(pieces[i].disponibilite === false){
+        nomsDisponibles.splice(i,1);
+        prixDisponibles.splice(i,1);
+    }
+}
+
+
+//création de la liste des pièces disponibles et leur prix
+const disponibleElement = document.createElement('ul');
+for(let i=0; i<nomsDisponibles.length; i++){
+    const nomPrixElement = document.createElement('li');
+    nomPrixElement.innerText = nomsDisponibles[i] +" - "+ prixDisponibles[i]+"€";
+    disponibleElement.appendChild(nomPrixElement);
+}
+document.querySelector(".disponible").appendChild(disponibleElement)
